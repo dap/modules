345af5035ac49c41205fab1149a976c2e4336e59
sts <sts@674913c4-1983-11de-b942-c947f6c0c824>
2009-09-20 19:30:50 +0000

    released 0.79

--- DateTime-Format-Natural/trunk/lib/DateTime/Format/Natural.pm
+++ DateTime-Format-Natural/trunk/lib/DateTime/Format/Natural.pm
@@ -16,7 +16,7 @@ use Params::Validate ':all';
 use Scalar::Util qw(blessed);
 use Storable qw(dclone);
 
-our $VERSION = '0.78_03';
+our $VERSION = '0.79';
 
 validation_options(
     on_fail => sub
